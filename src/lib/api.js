15c77b73d91d4438a299c8d656df08186e8860aa
Sasi-51 <knamasvi30@gmail.com>
2026-09-16 20:44:13 +0530

    Add files via upload

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
-export const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const API_BASE = 'https://bhoomi-7e7h.onrender.com';
 
-const api = axios.create({ baseURL: `${API_BASE}/api` });
+const api = axios.create({
+  baseURL: `${API_BASE}/api`,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  timeout: 30000
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('bhoomi_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
